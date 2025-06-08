25e3e657799821466c1e828a2d97b92c404f8834
Israt Anjuman Mumu <isratanjuman3@gmail.com>
2025-06-08 22:36:00 +0600

    added

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ console.log(process.env.DB_USER);
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rqq4klv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rqq4klv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
